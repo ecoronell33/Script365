d11ff2cc0ce42b895c89c836ba4be7503330b080
Erik Coronel <ecoronellw@gmail.com>
2026-05-10 12:39:43 -0500

    Add ForceChangePassword script for users
    This script connects to Microsoft Graph and updates user password profiles to require a password change at next sign-in.

---
+++ Microsoft Graph/Forcechangepassoword.ps1
@@ -0,0 +1,17 @@
+Connect-MgGraph -Scopes User.ReadWrite.All,Directory.AccessAsUser.All
+
+$passwordProfile = @{
+
+ForceChangePasswordNextSignIn = $true
+
+}
+
+$users = Import-Csv "‪D:\passwordchange.csv"
+
+$users | ForEach-Object {
+
+Write-Host "Updating $($_.UserPrincipalName)..." -ForegroundColor Yellow
+
+Update-MgUser -UserId $_.UserPrincipalName -PasswordProfile $passwordProfile
+
+}
